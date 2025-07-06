62dcc185797f3ca8851488434e4241f0ee3f9962
araful39 <rajuslam39@gmail.com>
2025-07-06 00:28:58 +0000

    json response add and checkt local host 1 wifi anouter pc http://192.168.0.159:3000/contract

--- index.js
+++ index.js
@@ -4,11 +4,33 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello  Express!');
+  res.send('Hello Raju Express!');
+});
+
+app.get('/about', (req, res) => {
+    console.log("GET /about hit");
+  res.send('About: I am flutter developer ad bdcalling in Softvence');
 });
 
 
 
-app.listen(PORT, () => {
+app.get('/contract', (req, res) => {
+  console.log("GET /contract hit");
+  res.json({
+    name: "Md Araful Islam",
+    email: "rajuslam39@gmail.com",
+    phone: "01928217679",
+    location: "Bangladesh",
+    profession: "Flutter Developer",
+    currentPosition: "Flutter Developer at bdCalling",
+    portfolio: "https://codecanyon.net/user/your_username",
+    totalProjects: 5,
+    platform: "CodeCanyon"
+  });
+});
+
+
+
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
